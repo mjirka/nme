59bfa13b7f9a030260eb50aaf01ee3fc1e0e5ebb
m-jirka <mrtnjrk@gmail.com>
2024-04-24 21:49:32 +0200

    11

--- _build/jupyter_execute/Ukol11.py
+++ _build/jupyter_execute/Ukol11.py
@@ -4,7 +4,7 @@
 # # Úkol 11
 
 # 1. Upravte řešení úlohy ze **Cvičení 11.01** tak, aby počáteční rychlost $v_{0}$ byla nalezena metodou půlení z Vámi zadaného intervalu $v_{0}\in \langle v_{0}^{\mathrm{min}},v_{0}^{\mathrm{max}}\rangle$. **(1.0 b)**
-# 2. Můžete-li, vyplňte prosím [anonymní dotazník](https://docs.google.com/forms/d/e/1FAIpQLSdb8CpgGCUmn25_Mg8kRmgkkU5MY-z7LwoRLAsNcqFg3RqIwA/viewform?usp=sf_link) (5 otázek).
+# 2. Můžete-li, vyplňte prosím [anonymní dotazník](https://docs.google.com/forms/d/e/1FAIpQLSe1ICT-1W0B2BBi3cVBug_nbA8Aa6nCNZmCZ6pkZPHuoEMlWg/viewform?usp=sf_link) (5 otázek).
 
 # In[1]:
 
